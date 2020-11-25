31af65aafdde7212fa91cb9d5d5de2f1a8b8c5ad
Mintae Kim <mkim221@myseneca.ca>
2020-11-25 14:25:43 -0500

    add new tests more

--- Tests/test_link.py
+++ Tests/test_link.py
@@ -25,3 +25,20 @@ def test_for_400(monkeypatch):
 
     monkeypatch.setattr(requests, "get", mock_url)
     assert dlf.single_link_status_checker("https://google.com") == 404
+
+
+# Test for printing color "Good" and "Bad"
+def test_good_link(capsys):
+    matchedString = "\x1b[32mPASSED [200] https://google.com - Good\n"
+    dlf.good_links("https://google.com", 200)
+    captured = capsys.readouterr()
+
+    assert captured.out == matchedString
+
+
+def test_bad_link(capsys):
+    matchedString = "\x1b[31mFAILED [400] https://google.com - Bad\n"
+    dlf.bad_links("https://google.com", 400)
+    captured = capsys.readouterr()
+
+    assert captured.out == matchedString
